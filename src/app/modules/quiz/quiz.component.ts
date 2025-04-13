81981474b5609f7fb522536d78e083ef7dba1dc2
Your Name <you@example.com>
2025-04-13 09:55:08 +0200

    --

--- src/app/modules/quiz/quiz.component.ts
+++ src/app/modules/quiz/quiz.component.ts
@@ -5,26 +5,42 @@ import { ConstQuestions } from 'src/app/constQuestions';
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
-  styleUrls: ['./quiz.component.scss']
+  styleUrls: ['./quiz.component.scss'],
 })
 export class QuizComponent {
-
   hostname: string = '';
   youtubeRef: string = ConstQuestions.youtubeRef;
   youtubeLink: SafeResourceUrl;
+  playedQuestions: number = 0;
 
   constructor(private sanitizer: DomSanitizer) {
-    this.youtubeLink = this.sanitizer.bypassSecurityTrustResourceUrl(this.youtubeRef);
+    this.youtubeLink = this.sanitizer.bypassSecurityTrustResourceUrl(
+      this.youtubeRef
+    );
 
-    if(JSON.parse(sessionStorage.getItem('host')|| '[]').length != 0){
-      this.hostname = JSON.parse(sessionStorage.getItem('host')|| '[]');
+    if (JSON.parse(sessionStorage.getItem('host') || '[]').length != 0) {
+      this.hostname = JSON.parse(sessionStorage.getItem('host') || '[]');
     }
 
-   }
+    setInterval(() => {
+      let questions: string[][] = JSON.parse(
+        sessionStorage.getItem('usedQuestionColors') || '[]'
+      );
+      let counter = 0;
+      for (let row of questions) {
+        for (let e of row) {
+          console.log(e);
+          if (e == '' || e == 'bonus') {
+            counter++;
+          }
+        }
+      }
+      this.playedQuestions = counter;
+    }, 1000);
+  }
 
-  public setHostname(input: string){
+  public setHostname(input: string) {
     this.hostname = input;
     sessionStorage.setItem('host', JSON.stringify(this.hostname));
   }
-
 }
